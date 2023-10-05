7c49f7f065f602fde7905a6a67b0c2ef7dc933e5
sai dorababu <khsd118194@gmail.com>
2023-10-05 06:11:48 +0530

    Dpll Implementation Successfully Done

--- dpll.py
+++ dpll.py
@@ -28,12 +28,12 @@ def findPureSymbol(clauses,symbols,values):
         flag = None
         for clause in clauses:
             for literal in clause:
-                if(literal == symbol):
+                if(literal == symbol and literal not in values.keys()):
                     if(flag == -1):
                         isPure = False
                         break
                     flag = 1
-                elif(literal == -symbol):
+                elif(literal == -symbol and -literal not in values.keys()):
                     if(flag == 1):
                         isPure = False
                         break
@@ -66,10 +66,8 @@ def dpll(clauses , symbols, values):
     #Early Termination
     #If anyone literal in all the clauses is true or not 
     if(checkAllTrue(clauses,symbols,values)):
+        print("valid ",values)
         return True
-    # for a,b in values.items():
-    #     print(a," : ",b)
-    # print()
     #If anyone clause is false 
     if(anyClauseIsFalse(clauses,symbols,values)):
         return False
@@ -78,49 +76,39 @@ def dpll(clauses , symbols, values):
     (symbol,value) = findPureSymbol(clauses,symbols,values)
     if value!=None:
         values[symbol] = value
+        symbols.remove(symbol)
         return dpll(clauses,symbols,values)
     
     literal = findUnitClause(clauses,symbols, values)
     if literal != None:
         if(literal<0):
             values[-literal] = False
+            symbols.remove(-literal)
         else:
             values[literal] = True
+            symbols.remove(literal)
         return dpll(clauses,symbols,values)
     
     # choose a literal
     literalReq = None
-    valueReq = None
-    for clause in clauses:
-        for literal in clause:
-            if(literal<0):
-                if(-literal not in values.keys()):
-                    literalReq = -literal
-                    valueReq = False
-                    break
-            else:
-                if(literal not in values.keys()):
-                    literalReq = literal
-                    valueReq = True
-                    break
-    if(literalReq == None):return False
-    values[literalReq] = valueReq
+    literalReq = symbols[0]
+    symbols.remove(literalReq)
+
+    values[literalReq] = True
     route1 = dpll(clauses,symbols,values)
-    values[literalReq] = not valueReq
+    values[literalReq] = False
     route2 = dpll(clauses,symbols,values)
     return route1 or route2
 
 clauses = [[1,2,3],[1,-2,3],[-1,2,3],[-1,-2,3],[1,2,-3],[1,-2,-3]]
+# clauses = [[1,2,3],[1,-2,3],[-1,2,3],[-1,-2,3],[1,2,-3],[1,-2,-3],[-1,2,-3]]
+clauses = [[1,2,3],[1,-2,3],[-1,2,3],[-1,-2,3],[1,2,-3],[1,-2,-3],[-1,2,-3],[-1,-2,-3]]
 symbols = [1,2,3]
 if __name__ == "__main__":
     values = {}
     ans = dpll(clauses,symbols,values);
-    print(ans)
     if(ans):
         print("The Given sentence is Satisfiable!")
-        print("Solution:")
-        for a,b in values.items():
-            print(a," : ",b)
     else:
         print("The Given Sentence is not satisfiable")
 
